7604d75d8c2509432eb185a44b4090fd3899350c
devsumanmdn <devsumanmdn@gmail.com>
2019-11-03 16:45:51 +0530

    formatting changes

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect, useRef } from "react";
-import PropTypes from "prop-types";
-import clsx from "clsx";
-import makeStyles from "@material-ui/styles/makeStyles";
-import withStyles from "@material-ui/styles/withStyles";
-import PlayIcon from "@material-ui/icons/PlayArrowRounded";
-import PlayNextIcon from "@material-ui/icons/SkipNextRounded";
-import PlayPreviousIcon from "@material-ui/icons/SkipPreviousRounded";
-import PauseIcon from "@material-ui/icons/PauseRounded";
-import MUISlider from "@material-ui/core/Slider";
-import VolumeOff from "@material-ui/icons/VolumeOff";
-import VolumeDown from "@material-ui/icons/VolumeDown";
-import VolumeMute from "@material-ui/icons/VolumeMute";
-import VolumeUp from "@material-ui/icons/VolumeUp";
-import * as mm from "music-metadata";
-import moment from "moment";
-import momentDurationFormatSetup from "moment-duration-format";
-import { connect } from "react-redux";
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import makeStyles from '@material-ui/styles/makeStyles';
+import withStyles from '@material-ui/styles/withStyles';
+import PlayIcon from '@material-ui/icons/PlayArrowRounded';
+import PlayNextIcon from '@material-ui/icons/SkipNextRounded';
+import PlayPreviousIcon from '@material-ui/icons/SkipPreviousRounded';
+import PauseIcon from '@material-ui/icons/PauseRounded';
+import MUISlider from '@material-ui/core/Slider';
+import VolumeOff from '@material-ui/icons/VolumeOff';
+import VolumeDown from '@material-ui/icons/VolumeDown';
+import VolumeMute from '@material-ui/icons/VolumeMute';
+import VolumeUp from '@material-ui/icons/VolumeUp';
+import * as mm from 'music-metadata';
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+import { connect } from 'react-redux';
 
-const fs = window.require("fs");
+const fs = window.require('fs');
 
 import {
   playSong as playSongAction,
@@ -26,29 +26,29 @@ import {
   playNextSong as playNextSongAction,
   playPrevSong as playPrevSongAction
   // setTotalDuration as setTotalDurationAction
-} from "../redux/player/playerActions";
+} from '../redux/player/playerActions';
 
 const Slider = withStyles({
   root: {
-    color: "#52af77",
+    color: '#52af77',
     height: 8
   },
   thumb: {
     height: 15,
     width: 15,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     // border: "2px solid currentColor",
-    transformOrigin: "center",
-    transform: "translate(0, 0)",
-    boxShadow: "0 0 4px 2px #fff6",
-    "&:hover,&$active": {
-      boxShadow: "inherit",
-      transform: "scale(1.2)"
+    transformOrigin: 'center',
+    transform: 'translate(0, 0)',
+    boxShadow: '0 0 4px 2px #fff6',
+    '&:hover,&$active': {
+      boxShadow: 'inherit',
+      transform: 'scale(1.2)'
     }
   },
   active: {},
   valueLabel: {
-    left: "calc(-50% + 4px)"
+    left: 'calc(-50% + 4px)'
   },
   track: {
     height: 4,
@@ -62,56 +62,66 @@ const Slider = withStyles({
 
 const useStyle = makeStyles({
   root: {
-    width: "100%",
-    maxHeight: 60,
-    minHeight: 60,
-    backgroundColor: "#222",
-    display: "flex",
-    alignItems: "center",
-    padding: "0 20px",
-    transitionDuration: "1s",
-    "&.expandedView": {
-      width: "100vw",
-      height: "100vh",
+    width: '100%',
+    maxHeight: 100,
+    minHeight: 100,
+    backgroundColor: '#222',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 20px',
+    transitionDuration: '1s',
+    '&.expandedView': {
+      width: '100vw',
+      height: '100vh',
       top: 0
     }
   },
   albumArt: {
     height: 50,
     width: 50,
-    backgroundColor: "#222",
+    backgroundColor: '#222',
     marginRight: 20
   },
   playBtn: {
-    backgroundColor: "transparent",
-    color: "#fff",
-    border: "1px solid #fff",
-    borderRadius: "50%",
+    backgroundColor: 'transparent',
+    color: '#fff',
+    border: '1px solid #fff',
+    borderRadius: '50%',
     height: 30,
     width: 30,
     marginRight: 10,
     padding: 0,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    outline: "none",
-    cursor: "pointer",
-    "& > svg": {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    outline: 'none',
+    cursor: 'pointer',
+    '& > svg': {
       fontSize: 18
     }
   },
   volumeContainer: {
-    display: "flex",
-    "& > svg": {
+    display: 'flex',
+    '& > svg': {
       marginRight: 10
     }
   },
-  songPlaybackProgress: {
+  infoContainer: {
     flexGrow: 1,
-    display: "flex",
-    "& > *": {
+    flexDirection: 'column',
+    '& > div': {
+      display: 'flex'
+    }
+  },
+  songInfo: {
+    '& > p': {
+      margin: 0
+    }
+  },
+  songPlaybackProgress: {
+    '& > *': {
       margin: 10,
-      alignItems: "center"
+      alignItems: 'center'
     }
   },
   progessSlider: {},
@@ -181,20 +191,20 @@ const Player = ({
       const songDataURL =
         songInfo && songInfo.format
           ? `data:audio/${songInfo.format.codec.toLowerCase()};base64,${data.toString(
-              "base64"
+              'base64'
             )}`
           : null;
       player.current = new Audio(songDataURL);
-      player.current.addEventListener("loadeddata", () => {
+      player.current.addEventListener('loadeddata', () => {
         setTotalDuration(player.current.duration);
       });
-      player.current.addEventListener("timeupdate", () => {
+      player.current.addEventListener('timeupdate', () => {
         setPlayedDuration(player.current.currentTime);
       });
-      player.current.addEventListener("volumechange", () => {
+      player.current.addEventListener('volumechange', () => {
         setVolume(player.current.volume);
       });
-      player.current.addEventListener("ended", () => {
+      player.current.addEventListener('ended', () => {
         playNextSong();
       });
       player.current.play();
@@ -224,60 +234,70 @@ const Player = ({
       : false;
 
   const albumArtDataURL = albumArt
-    ? `data:image/jpeg;base64,${albumArt.data.toString("base64")}`
+    ? `data:image/jpeg;base64,${albumArt.data.toString('base64')}`
     : undefined;
 
   return (
     <div className={clsx(classes.root, { expandedView })}>
       {songInfo ? (
         <img
-          role={"presentation"}
+          role={'presentation'}
           onClick={() => setExpadedView(!expandedView)}
           className={classes.albumArt}
           src={albumArtDataURL}
-          alt={"albumArt"}
+          alt={'albumArt'}
         />
       ) : (
-        <div className={classes.albumArt} style={{ background: "#000" }} />
+        <div className={classes.albumArt} style={{ background: '#000' }} />
       )}
       <button
-        type={"button"}
+        type={'button'}
         onClick={playPrevSong}
         className={classes.playBtn}
       >
         <PlayPreviousIcon />
       </button>
       <button
-        type={"button"}
+        type={'button'}
         onClick={handlePlayPause}
         className={classes.playBtn}
       >
         {playing ? <PauseIcon /> : <PlayIcon />}
       </button>
       <button
-        type={"button"}
+        type={'button'}
         onClick={playNextSong}
         className={classes.playBtn}
       >
         <PlayNextIcon />
       </button>
-      <div className={classes.songPlaybackProgress}>
-        <span>
-          {moment.duration(totalDuration, "seconds").format("mm:ss", {
-            trim: false
-          })}
-        </span>
-        <Slider
-          classes={{ root: classes.progessSlider }}
-          value={(playedDuration / totalDuration) * 100}
-          aria-labelledby={"continuous-slider"}
-          onChange={handleSeek}
-        />
-        <span>
-          {moment.duration(playedDuration, "seconds").format("mm:ss", {
-            trim: false
-          })}
-        </span>
+      <div className={classes.infoContainer}>
+        <div className={classes.songInfo}>
+          {songInfo ? (
+            <>
+              <p>{songInfo.common.title}</p>
+              <p>{songInfo.common.artists}</p>
+            </>
+          ) : null}
+        </div>
+        <div className={classes.songPlaybackProgress}>
+          <span>
+            {moment.duration(totalDuration, 'seconds').format('mm:ss', {
+              trim: false
+            })}
+          </span>
+          <Slider
+            classes={{ root: classes.progessSlider }}
+            value={(playedDuration / totalDuration) * 100}
+            aria-labelledby={'continuous-slider'}
+            onChange={handleSeek}
+          />
+          <span>
+            {moment.duration(playedDuration, 'seconds').format('mm:ss', {
+              trim: false
+            })}
+          </span>
+        </div>
       </div>
       <div className={classes.volumeContainer}>
         <VolumeUp />
@@ -285,7 +305,7 @@ const Player = ({
           classes={{ root: classes.volumeSlider }}
           value={volume * 100}
           onChange={handleChange}
-          aria-labelledby={"continuous-slider"}
+          aria-labelledby={'continuous-slider'}
         />
       </div>
     </div>
