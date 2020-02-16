b6ea6e6d4f14746de8c0b923c5d2904aefe608e8
devsumanmdn <devsumanmdn@gmail.com>
2020-02-16 18:17:02 +0530

    fix eslint error

--- src/util/parseFile.js
+++ src/util/parseFile.js
@@ -1,7 +1,8 @@
 import * as mm from 'music-metadata';
 
 export default location =>
-  mm.parseFile(location).then(info => {
+  mm.parseFile(location).then(parsedData => {
+    const info = { ...parsedData };
     if (!(info.common && info.common.title)) {
       const filename = location.split('/').slice(-1)[0];
       info.common.title = filename;
